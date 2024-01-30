8085dbaceb5d5791ba6af540eade297d4f2029ac
MD JASIM UDDIN <mjunachole97@gmail.com>
2024-01-30 21:47:51 +0600

    conver integer & float

---
+++ convert-number.js
@@ -0,0 +1,13 @@
+var orangePrice = 350;
+var applePrice = 100;
+var internetBill = '525'
+var mobileRecharge = "185.89"
+var totalPrice = orangePrice + applePrice;
+// convert String to Integer Number
+var internel = parseInt(internetBill)
+
+// convert String to Float Number
+var mobileBill = parseFloat(mobileRecharge)
+
+var totalBill = totalPrice + internel + mobileBill;
+console.log(totalBill)
