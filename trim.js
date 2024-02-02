7605be76f20591a3ee3b3d84c572d3c82745ff24
MD JASIM UDDIN <mjunachole97@gmail.com>
2024-02-02 22:54:05 +0600

    Trim() method use

---
+++ trim.js
@@ -0,0 +1,11 @@
+// Trim method use
+
+const name = "    jasim";
+const name2 = "jasim  ";
+
+if(name.trim()==name2.trim()){
+    console.log("Two Value is same");
+}
+else{
+    console.log("two value is not same")
+}
