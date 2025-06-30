ebe3d81be3ad539bcc690db0c3a3e49073195563
MD JASIM UDDIN <mjunachole97@gmail.com>
2025-07-01 00:51:05 +0600

    event practice

---
+++ JavaScript_Object.js/JsEvent.js
@@ -0,0 +1,28 @@
+function clickEvent() {
+    console.log("Button click done");
+}
+
+
+function myEventInfo() {
+    console.log("Mouse over is a javaScript event method.");
+}
+
+
+function mouseOutEvent() {
+    console.log("Mouse Out.");
+}
+
+
+
+function OnKeyDown() {
+    console.log("Keyboard Key down.");
+}
+
+function OnKeyUp() {
+    console.log("Keyboard Key Up");
+}
+
+function loadingSuccessful() {
+    alert("Full Website Loading Successful");
+}
+
